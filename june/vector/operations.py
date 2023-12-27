ab96e293957998d0b9f37612976c203d1d3136e6
Chirag Juneja <chiragjuneja6@gmail.com>
2023-12-28 00:24:05 +0530

    vector subtraction and exception test

--- june/vector/operations.py
+++ june/vector/operations.py
@@ -1,6 +1,11 @@
 from .types import * 
 
-def add(v:Vector,w:Vector):
+def add(v:Vector,w:Vector)->Vector:
    if len(v)!=len(w):
-      raise RuntimeError(f"Vector dimentions must match. Got {len(v)} and {len(w)}")
+      raise RuntimeError(f"Vector dimentions must match. Got {len(v)} and {len(w)}.")
    return [vi+wi for vi,wi in zip(v,w)]
+
+def subtract(v:Vector,w:Vector)->Vector:
+   if len(v)!=len(w):
+      raise RuntimeError(f"Vector dimentions must match. Got {len(v)} and {len(w)}.")
+   return [vi-wi for vi,wi in zip(v,w)]
--- test/june/vector/test_operation.py
+++ test/june/vector/test_operation.py
@@ -1,3 +1,4 @@
+import pytest
 from june import vector
 
 def test_add():
@@ -5,3 +6,22 @@ def test_add():
     w = [1,2,3] 
     result = vector.add(v,w)
     assert result == [2.0, 4.0, 6.0]
+
+def test_add_invalid_size():
+    v = [1.0,2.0,3.0]
+    w = [1,2,3,4] 
+    with pytest.raises(RuntimeError):
+        result = vector.add(v,w)
+
+def test_subtract():
+    v = [1.0,2.0,3.0]
+    w = [1,2,3] 
+    result = vector.subtract(v,w)
+    assert result == [0.0, 0.0, 0.0]
+
+def test_subtract_invalid_size():
+    v = [1.0,2.0,3.0]
+    w = [1,2,3,4] 
+    with pytest.raises(RuntimeError):
+        result = vector.subtract(v,w)
+
