fb8a8b74824da0a892a1a292522ee62b370009e0
Chirag Juneja <chiragjuneja6@gmail.com>
2024-01-01 23:26:55 +0530

    vector sum and scalar mul

--- june/vector/operations.py
+++ june/vector/operations.py
@@ -1,11 +1,21 @@
 from .types import * 
+import builtins
+
 
 def add(v:Vector,w:Vector)->Vector:
    if len(v)!=len(w):
-      raise RuntimeError(f"Vector dimentions must match. Got {len(v)} and {len(w)}.")
+      raise RuntimeError(f"Vector dimensions must match. Got {len(v)} and {len(w)}.")
    return [vi+wi for vi,wi in zip(v,w)]
 
 def subtract(v:Vector,w:Vector)->Vector:
    if len(v)!=len(w):
-      raise RuntimeError(f"Vector dimentions must match. Got {len(v)} and {len(w)}.")
+      raise RuntimeError(f"Vector dimensions must match. Got {len(v)} and {len(w)}.")
    return [vi-wi for vi,wi in zip(v,w)]
+
+def sum(vectors:List[Vector])->Vector:
+   if not all([len(v)==len(vectors[0]) for v in vectors]):
+      raise RuntimeError(f"Vector dimensions must match. Got {[len(v) for v in vectors]}.")
+   return [builtins.sum(v[i] for v in vectors) for i in range(len(vectors[0]))]
+
+def mul(vector:Vector,scalar:float)->Vector:
+   return [scalar*vi for vi in vector]
--- test/june/vector/test_operation.py
+++ test/june/vector/test_operation.py
@@ -25,3 +25,22 @@ def test_subtract_invalid_size():
     with pytest.raises(RuntimeError):
         result = vector.subtract(v,w)
 
+def test_sum():
+    v = [1.0,2.0,3.0]
+    w = [1,2,3]
+    x=[1,2,3] 
+    result = vector.sum([v,w,x])
+    assert result == [3.0, 6.0, 9.0]
+
+def test_sum_invalid_size():
+    v = [1.0,2.0,3.0]
+    w = [1,2,3,4] 
+    x=[1,2,3] 
+    with pytest.raises(RuntimeError):
+        result = vector.sum([v,w,x])
+
+def test_mul():
+    v = [1,2,3]
+    result = vector.mul(v,5)
+    assert result == [5.,10.,15.]
+
