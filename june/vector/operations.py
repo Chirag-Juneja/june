e8ac503c52c0e5fb43525236e7de3dc924b6c871
Chirag Juneja <chiragjuneja6@gmail.com>
2024-01-02 23:20:32 +0530

    vector mag dot mul distance

--- june/vector/operations.py
+++ june/vector/operations.py
@@ -1,5 +1,6 @@
-from .types import * 
 import builtins
+import math
+from .types import * 
 
 
 def add(v:Vector,w:Vector)->Vector:
@@ -19,3 +20,23 @@ def sum(vectors:List[Vector])->Vector:
 
 def mul(vector:Vector,scalar:float)->Vector:
    return [scalar*vi for vi in vector]
+
+def mean(vectors:List[Vector])->Vector:
+   if not all([len(v)==len(vectors[0]) for v in vectors]):
+      raise RuntimeError(f"Vector dimensions must match. Got {[len(v) for v in vectors]}.")
+   n = len(vectors)
+   return mul(sum(vectors),1/n)
+
+def dot(v:Vector,w:Vector)->float:
+   if len(v)!=len(w):
+      raise RuntimeError(f"Vector dimensions must match. Got {len(v)} and {len(w)}.")
+   return builtins.sum(vi*wi for vi,wi in zip(v,w))
+
+def magnitude(v:Vector):
+   return math.sqrt(dot(v,v))
+
+def distance(v:Vector,w:Vector)->float:
+   if len(v)!=len(w):
+      raise RuntimeError(f"Vector dimensions must match. Got {len(v)} and {len(w)}.")
+   sub = subtract(v,w)
+   return magnitude(sub)
--- test/june/vector/test_operation.py
+++ test/june/vector/test_operation.py
@@ -44,3 +44,45 @@ def test_mul():
     result = vector.mul(v,5)
     assert result == [5.,10.,15.]
 
+def test_mean():
+    v = [1.0,2.0,3.0]
+    w = [1,2,3]
+    x=[1,2,3] 
+    result = vector.mean([v,w,x])
+    assert result == [1.0, 2.0, 3.0]
+
+def test_mean_invalid_size():
+    v = [1.0,2.0,3.0]
+    w = [1,2,3,4] 
+    x=[1,2,3] 
+    with pytest.raises(RuntimeError):
+        result = vector.mean([v,w,x])
+
+def test_dot():
+    v = [1.0,2.0,3.0]
+    w =[4,5,6]
+    result = vector.dot(v,w)
+    assert result == 32
+
+def test_dot_invalid_size():
+    v = [1.0,2.0,3.0]
+    w = [1,2,3,4] 
+    with pytest.raises(RuntimeError):
+        result = vector.dot(v,w)
+
+def test_magnitude():
+    v=[3,4]
+    result = vector.magnitude(v)
+    assert result == 5
+
+def test_distance():
+    v=[4,5]
+    w=[1,1]
+    result = vector.distance(v,w)
+    assert result == 5
+
+def test_distance_invalid_size():
+    v = [1.0,2.0,3.0]
+    w = [1,2,3,4] 
+    with pytest.raises(RuntimeError):
+        result = vector.dot(v,w)
