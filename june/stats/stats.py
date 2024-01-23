1bc0821242a7eb3c70b52f3138ae8d45f2f26f76
Chirag Juneja <chiragjuneja6@gmail.com>
2024-01-23 21:25:24 +0530

    covariance and correlation

--- june/stats/stats.py
+++ june/stats/stats.py
@@ -1,3 +1,4 @@
+from june import vector
 from june.vector import Vector
 from collections import Counter
 import math
@@ -41,3 +42,20 @@ def standard_deviation(v:Vector)->float:
 
 def interquartile_range(v:Vector)->float:
     return quantile(v,0.75)-quantile(v,0.25)
+
+def covariance(v:Vector,w:Vector)->float:
+    if len(v)!=len(w):
+        raise RuntimeError(f"Vector dimensions must match. Got {len(v)} and {len(w)}.")
+    if len(v)<2:
+        raise RuntimeError("Vector should have at least 2 elements.")
+    v_mean = mean(v)
+    w_mean = mean(w)
+    v_deviations,w_deviations = [x-v_mean for x in v],[x-w_mean for x in w]
+    return vector.dot(v_deviations,w_deviations)/(len(v)-1)
+
+def correlation(v:Vector,w:Vector)->float:
+    v_std = standard_deviation(v)
+    w_std = standard_deviation(w)
+    if w_std > 0 and v_std > 0:
+        return covariance(v,w)/v_std/w_std
+    return 0
--- test/june/stats/test_stats.py
+++ test/june/stats/test_stats.py
@@ -1,57 +1,82 @@
 import pytest
 import math
 import statistics
-from june import vector
+from june.vector import Vector
 from june import stats
 
 
 def test_mean():
-    v = vector.Vector([1,2,3,4,5])
+    v = Vector([1,2,3,4,5])
     assert stats.mean(v) == 3.0
 
 def test_median_odd_len():
-    v = vector.Vector([1,2,3,4,5])
+    v = Vector([1,2,3,4,5])
     assert stats.median(v)==3.0
 
 def test_median_even_len():
-    v = vector.Vector([1,2,3,4,5,6])
+    v = Vector([1,2,3,4,5,6])
     assert stats.median(v)==3.5
 
 def test_median_empty():
-    v = vector.Vector([])
+    v = Vector([])
     with pytest.raises(RuntimeError):
         stats.median(v)
 
 def test_quantile():
-    v = vector.Vector([1,1,2,2,3,4,5,6,7,8])
+    v = Vector([1,1,2,2,3,4,5,6,7,8])
     assert stats.quantile(v,0.2)==2.0
     assert stats.quantile(v,0.8)==7.0
     assert stats.quantile(v,0.9)==8.0
 
 def test_mode():
-    v = vector.Vector([1,1,2,2,3,4,5,6,7,8])
+    v = Vector([1,1,2,2,3,4,5,6,7,8])
     assert stats.mode(v)==1.0
     assert stats.mode(v[1:])==2.0
 
 def test_range():
-    v = vector.Vector([3,4,5])
+    v = Vector([3,4,5])
     result =stats.drange(v)
     assert result == 2.0
 
 def test_variance():
     l = [1,1,2,2,3,4,5,6,7,8]
-    v = vector.Vector(l)
+    v = Vector(l)
     result = stats.variance(v)
     assert round(result,5) == round(statistics.variance(l),5)
 
 def test_standard_deviation():
     l = [1,1,2,2,3,4,5,6,7,8]
-    v = vector.Vector(l)
+    v = Vector(l)
     result = stats.standard_deviation(v)
     assert round(result,5) == round(math.sqrt(statistics.variance(l)),5)
 
 def test_interquartile_range():
     l = [1,2,3,4,5,6,7,8,9,10]
-    v= vector.Vector(l)
+    v= Vector(l)
     result = stats.interquartile_range(v)
     assert result == 5
+
+def test_covariance():
+    l = [1,1,2,2,3,4,5,6,7,8]
+    m = [-1,-1,2,3,4,-4,-5,6,7,8]
+    result = stats.covariance(Vector(l),Vector(m))
+    assert round(statistics.covariance(l,m),5) == round(result,5)
+
+def test_covariance_unequal():
+    l = [1,1,2,2,3,4,5,6,7,8]
+    m = [1,2,3,4,-4,-5,6,7,8]
+    with pytest.raises(RuntimeError):
+        stats.covariance(Vector(l),Vector(m))
+    
+def test_correlation():
+    l = [1,1,2,2,3,4,5,6,7,8]
+    m = [-1,-1,2,3,4,-4,-5,6,7,8]
+    result = stats.correlation(Vector(l),Vector(m))
+    assert round(statistics.correlation(l,m),5) == round(result,5)
+
+def test_correlation_unequal():
+    l = [1,1,2,2,3,4,5,6,7,8]
+    m = [1,2,3,4,-4,-5,6,7,8]
+    with pytest.raises(RuntimeError):
+        stats.correlation(Vector(l),Vector(m))
+    
